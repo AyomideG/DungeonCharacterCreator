6c7a2729f83fbe5dffae6b909cfa4b2c4df6e76f
samwheeler2014 <swheele2@oswego.edu>
2018-12-01 13:09:02 -0500

    Fixing Background Page

--- app/src/main/java/yeet/dungeonsomething/dungeoncharactercreator/Views/CharacterBGActivity.java
+++ app/src/main/java/yeet/dungeonsomething/dungeoncharactercreator/Views/CharacterBGActivity.java
@@ -5,9 +5,11 @@ import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.TypedValue;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -20,11 +22,28 @@ public class CharacterBGActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_bg);
 
-        ListView listview = (ListView) findViewById(R.id.persview);
-        String [] traits = new String[] {"kind", "clever", "honest"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, traits);
-        listview.setAdapter(adapter);
+        TextView traitsview = (TextView) findViewById(R.id.persview);
+        String[] traits = new String[]{"kind", "clever", "honest"};
+        for (String t : traits) {
+            if (t.equals(traits[0])) {
+                traitsview.setText(traitsview.getText() + t);
+            } else {
+                traitsview.setText(traitsview.getText() + "\n" + t);
+            }
+        }
+       // ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+           //     android.R.layout.simple_list_item_1, traits);
+        //listview.setAdapter(adapter);
+        /*listview.setOnTouchListener(new View.OnTouchListener() {
+            // Setting on Touch Listener for handling the touch inside ScrollView
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // Disallow the touch request for parent scroll on touch of child view
+                v.getParent().requestDisallowInterceptTouchEvent(true);
+                return false;
+            }
+        });*/
+        //setListViewHeightBasedOnChildren(listview);
 
     }
 
@@ -33,8 +52,32 @@ public class CharacterBGActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void showStats(View view){
+    public void showStats(View view) {
         Intent intent = new Intent(this, StatsActivity.class);
         startActivity(intent);
     }
+
+    /**** Method for Setting the Height of the ListView dynamically.
+     **** Hack to fix the issue of not showing all the items of the ListView
+     **** when placed inside a ScrollView  ****/
+   /* public static void setListViewHeightBasedOnChildren(ListView listView) {
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null)
+            return;
+
+        int desiredWidth = View.MeasureSpec.makeMeasureSpec(listView.getWidth(), View.MeasureSpec.UNSPECIFIED);
+        int totalHeight = 0;
+        View view = null;
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            view = listAdapter.getView(i, view, listView);
+            if (i == 0)
+                view.setLayoutParams(new ViewGroup.LayoutParams(desiredWidth, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+            view.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
+            totalHeight += view.getMeasuredHeight();
+        }
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
+    }*/
 }
