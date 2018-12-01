dd25504343390622c53229aa905952e0a0c0c7db
samwheeler2014 <swheele2@oswego.edu>
2018-12-01 15:53:04 -0500

    Loading Character - Background

--- app/src/main/java/yeet/dungeonsomething/dungeoncharactercreator/Model/Language.java
+++ app/src/main/java/yeet/dungeonsomething/dungeoncharactercreator/Model/Language.java
@@ -8,4 +8,44 @@ public class Language implements Serializable {
     String[] typical_speakers;
     String script;
     String url;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String[] getTypical_speakers() {
+        return typical_speakers;
+    }
+
+    public void setTypical_speakers(String[] typical_speakers) {
+        this.typical_speakers = typical_speakers;
+    }
+
+    public String getScript() {
+        return script;
+    }
+
+    public void setScript(String script) {
+        this.script = script;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
--- app/src/main/java/yeet/dungeonsomething/dungeoncharactercreator/Views/CharacterBGActivity.java
+++ app/src/main/java/yeet/dungeonsomething/dungeoncharactercreator/Views/CharacterBGActivity.java
@@ -17,6 +17,8 @@ import android.widget.TextView;
 import yeet.dungeonsomething.dungeoncharactercreator.APIDataManager;
 import yeet.dungeonsomething.dungeoncharactercreator.CharacterManager;
 import yeet.dungeonsomething.dungeoncharactercreator.Model.Character;
+import yeet.dungeonsomething.dungeoncharactercreator.Model.Feature;
+import yeet.dungeonsomething.dungeoncharactercreator.Model.Language;
 import yeet.dungeonsomething.dungeoncharactercreator.Model.PlayerInfo;
 import yeet.dungeonsomething.dungeoncharactercreator.Model.Statistics;
 import yeet.dungeonsomething.dungeoncharactercreator.Model.Trait;
@@ -25,6 +27,7 @@ import yeet.dungeonsomething.dungeoncharactercreator.R;
 public class CharacterBGActivity extends AppCompatActivity {
 
     private Character myCharacter;
+    private boolean ifNull = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +48,8 @@ public class CharacterBGActivity extends AppCompatActivity {
             myCharacter.setMyclass(APIDataManager.getInstance(getAssets()).getClasses()[0]);
             myCharacter.setLevel(1);
             myCharacter.setPlayerInfo(new PlayerInfo());
+            CharacterManager.getInstance(getAssets()).newCharacter(myCharacter);
+            ifNull = true;
         }
 
     }
@@ -58,12 +63,68 @@ public class CharacterBGActivity extends AppCompatActivity {
         ((TextView) findViewById(R.id.character_bg_Level)).setText(String.valueOf(myCharacter.getLevel()));
         ((TextView) findViewById(R.id.character_bg_background)).setText(myCharacter.getPlayerInfo().getBackGround());
         ((TextView) findViewById(R.id.character_bg_alignment)).setText(myCharacter.getPlayerInfo().getAlignment());
-        TextView traitsView = findViewById(R.id.character_bg_traits);
-        for (Trait t : myCharacter.getPlayerInfo().getTraits()) {
-            if (t.getName().equals(myCharacter.getPlayerInfo().getTraits().get(0))) {
-                traitsView.setText(traitsView.getText() + t.getName());
-            } else {
-                traitsView.setText(traitsView.getText() + "\n" + t.getName());
+        if (myCharacter.getPlayerInfo().getTraits() == null) {
+            if (myCharacter.getPlayerInfo().getFeatures() == null) {
+                if (myCharacter.getPlayerInfo().getBonds() == null) {
+                    if (myCharacter.getPlayerInfo().getFlaws() == null) {
+                        if (myCharacter.getPlayerInfo().getIdeals() == null) {
+                            if (myCharacter.getPlayerInfo().getLanguages() == null) {
+                                ifNull = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (!ifNull) {
+            TextView traitsView = findViewById(R.id.character_bg_traits);
+            for (Trait t : myCharacter.getPlayerInfo().getTraits()) {
+                if (t.getName().equals(myCharacter.getPlayerInfo().getTraits().get(0))) {
+                    traitsView.setText(traitsView.getText() + t.getName());
+                } else {
+                    traitsView.setText(traitsView.getText() + "\n" + t.getName());
+                }
+            }
+            TextView featureView = findViewById(R.id.character_bg_features);
+            for (Feature f : myCharacter.getPlayerInfo().getFeatures()) {
+                if (f.getName().equals(myCharacter.getPlayerInfo().getFeatures().get(0))) {
+                    featureView.setText(featureView.getText() + f.getName());
+                } else {
+                    featureView.setText(featureView.getText() + "\n" + f.getName());
+                }
+            }
+            TextView idealsView = findViewById(R.id.character_bg_ideals);
+            for (String i : myCharacter.getPlayerInfo().getIdeals()) {
+                if (i.equals(myCharacter.getPlayerInfo().getIdeals().get(0))) {
+                    idealsView.setText(idealsView.getText() + i);
+                } else {
+                    idealsView.setText(idealsView.getText() + "\n" + i);
+                }
+            }
+            TextView bondsView = findViewById(R.id.character_bg_bonds);
+            for (String b : myCharacter.getPlayerInfo().getBonds()) {
+                if (b.equals(myCharacter.getPlayerInfo().getBonds().get(0))) {
+                    bondsView.setText(bondsView.getText() + b);
+                } else {
+                    bondsView.setText(bondsView.getText() + "\n" + b);
+                }
+            }
+            TextView flawsView = findViewById(R.id.character_bg_flaws);
+            for (String f : myCharacter.getPlayerInfo().getFlaws()) {
+                if (f.equals(myCharacter.getPlayerInfo().getFlaws().get(0))) {
+                    flawsView.setText(flawsView.getText() + f);
+                } else {
+                    flawsView.setText(flawsView.getText() + "\n" + f);
+                }
+            }
+            TextView languageView = findViewById(R.id.character_bg_languages);
+            for (Language l : myCharacter.getPlayerInfo().getLanguages()) {
+                if (l.getName().equals(myCharacter.getPlayerInfo().getLanguages().get(0))) {
+                    languageView.setText(languageView.getText() + l.getName());
+                } else if (l.getName() != null && !l.getName().isEmpty()) {
+                } else {
+                    languageView.setText(languageView.getText() + "\n" + l.getName());
+                }
             }
         }
     }
