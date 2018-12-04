b2221114fee01ba766a222fd9c9c980e81b8a1ef
Ayomide Gbadamosi <34720960+AyomideG@users.noreply.github.com>
2018-12-03 19:45:10 -0500

    Add files via upload

---
+++ app/src/main/java/yeet/dungeonsomething/dungeoncharactercreator/Views/EListAdapter.java
@@ -0,0 +1,149 @@
+package yeet.dungeonsomething.dungeoncharactercreator.Views;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.CheckBox;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import yeet.dungeonsomething.dungeoncharactercreator.R;
+
+public class EListAdapter extends BaseExpandableListAdapter implements ExpandableListView.OnChildClickListener {
+
+    private Context context;
+    private ArrayList<Group> groups;
+
+    public EListAdapter(Context context, ArrayList<Group> groups) {
+        this.context = context;
+        this.groups = groups;
+    }
+
+    public Object getChild(int groupPosition, int childPosition) {
+        return groups.get(groupPosition).getChildItem(childPosition);
+    }
+
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    public int getChildrenCount(int groupPosition) {
+        return groups.get(groupPosition).getChildrenCount();
+    }
+
+    public Object getGroup(int groupPosition) {
+        return groups.get(groupPosition);
+    }
+
+    public int getGroupCount() {
+        return groups.size();
+    }
+
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        Group group = (Group) getGroup(groupPosition);
+
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.settings_list_item_parent, null);
+        }
+
+        TextView tv = (TextView) convertView.findViewById(R.id.tvGroup);
+        tv.setText(group.getParent());
+
+        CheckBox checkBox = (CheckBox) convertView.findViewById(R.id.chbGroup);
+        checkBox.setChecked(group.getChecked());
+
+        checkBox.setOnClickListener(new Group_CheckBox_Click(groupPosition));
+
+        return convertView;
+    }
+
+    class Group_CheckBox_Click implements View.OnClickListener {
+        private int groupPosition;
+
+        Group_CheckBox_Click(int groupPosition) {
+            this.groupPosition = groupPosition;
+        }
+
+        public void onClick(View v) {
+            groups.get(groupPosition).toggle();
+
+            int childrenCount = groups.get(groupPosition).getChildrenCount();
+            boolean groupIsChecked = groups.get(groupPosition).getChecked();
+            for (int i = 0; i < childrenCount; i++)
+                groups.get(groupPosition).getChildItem(i).setChecked(groupIsChecked);
+
+            notifyDataSetChanged();
+        }
+    }
+
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        Child child = groups.get(groupPosition).getChildItem(childPosition);
+
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.settings_list_item_child, null);
+        }
+
+        TextView tv = (TextView) convertView.findViewById(R.id.tvChild);
+        tv.setText(child.getValuess());
+
+        CheckBox checkBox = (CheckBox) convertView.findViewById(R.id.chbChild);
+        checkBox.setChecked(child.getChecked());
+
+        checkBox.setOnClickListener(new Child_CheckBox_Click(groupPosition, childPosition));
+
+        return convertView;
+    }
+
+    class Child_CheckBox_Click implements View.OnClickListener {
+        private int groupPosition;
+        private int childPosition;
+
+        Child_CheckBox_Click(int groupPosition, int childPosition) {
+            this.groupPosition = groupPosition;
+            this.childPosition = childPosition;
+        }
+
+        public void onClick(View v) {
+            handleClick(childPosition, groupPosition);
+        }
+    }
+
+    public void handleClick(int childPosition, int groupPosition) {
+        groups.get(groupPosition).getChildItem(childPosition).toggle();
+
+        int childrenCount = groups.get(groupPosition).getChildrenCount();
+        boolean childrenAllIsChecked = true;
+        for (int i = 0; i < childrenCount; i++) {
+            if (!groups.get(groupPosition).getChildItem(i).getChecked())
+                childrenAllIsChecked = false;
+        }
+
+        groups.get(groupPosition).setChecked(childrenAllIsChecked);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+        handleClick(childPosition, groupPosition);
+        return true;
+    }
+
+}
